d3d97c00d2eb048e9666c35a2d01a814916795cf
whoamins <40am1ns@protonmail.com>
2021-10-28 18:25:31 +0300

    Rewritten using ViewBinding

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -1,39 +1,26 @@
-    package com.example.dice
+package com.example.dice
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         rollDice()
-        /*
 
-        val rollButton: Button = findViewById(R.id.button3)
-
-        rollButton.setOnClickListener {
-              rollDice()
-        }
-
-         */
-
-        // TODO: Re-write this with ViewBinding
-
-        findViewById<Button?>(R.id.button).setOnClickListener {
-            rollDice()
-        }
+        binding.button.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        // Create new Dice object with sides and roll it
         val dice = Dice(numSides = 6)
-
-        // Try to refactor this code
-
 
         // Determine which drawable resource should be used based on dice roll result
         val imageResource = when (dice.roll()) {
@@ -46,6 +33,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        findViewById<ImageView?>(R.id.imageView).setImageResource(imageResource)
+        binding.imageView.setImageResource(imageResource)
     }
 }
